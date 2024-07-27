e1e259b69545c0eb0d0d45d056709d9095862d08
callejero666 <rodrigocjsreyes666@gmail.com>
2024-07-26 21:16:45 -0300

    creacion

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Principal } from './componentes/principal';
+import { Registro } from './componentes/Registro';
+import { Navbar } from './componentes/Navbar';
+import './App.css';
+import { Login } from './componentes/login';
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Principal />} />
+                <Route path="/principal" element={<Principal />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/registro" element={<Registro />} />
+            </Routes>
+        </Router>
+    );
+}
+// implementar
+
+export default App;
+
+
